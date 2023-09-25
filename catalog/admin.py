bdedc6570fb6b323b88a601e72a10fd957cbcf8e
west5660 <westw_56@mail.ru>
2023-09-25 23:46:11 +0300

    make admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+from atexit import register
 
+from django.contrib import admin
+from .models import *
 # Register your models here.
+
+admin.site.register(Diagnoz)
+admin.site.register(Check)
+admin.site.register(Spec)
+admin.site.register(Pacient)
+admin.site.register(Zapisi)
+# admin.site.register(Kino)
+# admin.site.register(Country)
+
+# class Diagnozadmin(admin.ModelAdmin):
+#     list_display_links = ('diagnoz')
+# admin.site.register(Diagnoz,Diagnozadmin)
+#
+# class Checkadmin(admin.ModelAdmin):
+#     list_display = ('sum_mat','sum_uslug')
+# admin.site.register(Check,Checkadmin)
+#
+# class Specadmin(admin.ModelAdmin):
+#     list_display_links = ('name','spec')
+# admin.site.register(Spec,Specadmin)
+#
+# class Pacientadmin(admin.ModelAdmin):
+#     list_display_links = ('name', 'adres', 'tel')
+# admin.site.register(Pacient,Pacientadmin)
+
--- catalog/models.py
+++ catalog/models.py
@@ -2,40 +2,51 @@ from django.db import models
 
 # Create your models here.
 class Diagnoz(models.Model):
-    diagnoz = models.CharField(max_length=40)
+    diagnoz = models.CharField(max_length=40, verbose_name='Диагноз')
 
     def __str__(self):
         return self.diagnoz
 
 class Check(models.Model):
-    sum_mat= models.CharField(max_length=40)
-    sum_uslug= models.CharField(max_length=40)
+    sum_mat= models.CharField(max_length=40, verbose_name='Материалы затраты')
+    sum_uslug= models.CharField(max_length=40, verbose_name='Цена услуг')
 
     def __str__(self):
-        return self.sum_uslug
+        return f'{self.sum_mat},{self.sum_uslug}'
 
 class Spec(models.Model):
-    name= models.CharField(max_length=40)
-    spec= models.CharField(max_length=40)
+    name= models.CharField(max_length=40, verbose_name='Имя доктора')
+    spec= models.CharField(max_length=40, verbose_name='Специальность доктора')
 
     def __str__(self):
         return self.name, self.spec
 
 class Pacient(models.Model):
-    name = models.CharField(max_length=40)
-    adres = models.CharField(max_length=40)
-    tel = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, verbose_name='Кличка пациента')
+    adres = models.CharField(max_length=40, verbose_name='Адрес')
+    tel = models.CharField(max_length=40, verbose_name='Телефон')
 
     def __str__(self):
         return self.name
 
 class Zapisi(models.Model):
-    num = models.IntegerField()
+    num = models.IntegerField(verbose_name='Номер записи')
     pacient = models.ForeignKey(Pacient, on_delete=models.SET_NULL, null=True)
     spec = models.ManyToManyField(Spec)
     num_vizit = models.IntegerField()
-    data_vizit = models.DateField(null=True, blank=False)
-    anamnez = models.CharField(max_length=40)
+    data_vizit = models.DateField(null=True, blank=False, verbose_name='Дата визита')
+    anamnez = models.CharField(max_length=40, verbose_name='Анамнез')
     diagnoz = models.ForeignKey(Diagnoz, on_delete=models.SET_NULL, null=True)
-    shema_lechenia = models.TextField(max_length=500)
+    shema_lechenia = models.TextField(max_length=500, verbose_name='Схема лечения')
     chek_uslug = models.ForeignKey(Check, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.num
+
+    # def display_pacient(self):
+    #     res = ''
+    #     for a in self.pacient.all():
+    #         res += a.lname + ' '
+    #     return res
+    #
+    # display_pacient.short_description = 'Пациенты'
